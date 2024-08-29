62c81cfcdd7adc95be6b62b7da64226da7254cf0
EnzoAndSo <grommeshjonathan@gmail.com>
2024-08-28 21:52:07 -0500

    added CRUD endpoints for categories. All tested succesfully in insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,21 +15,44 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
   // find one category by its `id` value
+  Category
+    .findByPk(req.params.id)
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category 
+    .create(req.body,)
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category 
+    .update(req.body, {where: {id: req.params.id,},})
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category 
+    .destroy({where: { id: req.params.id}})
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
 });
 
 module.exports = router;
