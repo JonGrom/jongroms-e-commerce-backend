6ee2dee139993646b6ec48a76b3d09909b42423f
EnzoAndSo <grommeshjonathan@gmail.com>
2024-08-28 22:35:41 -0500

    created all CRUD endpoints for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,58 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag 
+    .findAll({
+      include: {model: Product}
+    })
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag
+    .findByPk(req.params.id, {
+      include: {model: Product}
+    })
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag 
+    .create(req.body,)
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag 
+    .update(req.body, {where: {id: req.params.id,},})
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag 
+    .destroy({where: { id: req.params.id}})
+    .then(response => {
+      res.json({status: 'success', payload: response})
+    })
+    .catch( err => res.status(500).json({status: 'error', payload: err.message}))
 });
 
 module.exports = router;
